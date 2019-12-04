e30f2e34aebd7863457bf39eb885ceff32c589ae
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-12-03 21:49:46 -0700

    Fixing connection for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 
-var Sequelize = require("sequelize");
+var mysql = require("mysql");
 
+var connection;
 
-var sequelize = new Sequelize("burgers_db", "root", "root", {
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+  user: "root",
+  passsword: "root",
+  database: "burgers_db"
+  });
+};
 
-module.exports = sequelize;
+connection.connect();
+module.exports = connection;
